227ef302ad1e0b37acbabe257f88523842838826
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2021-12-24 18:39:57 +0900

    login finish

--- components/Login.js
+++ components/Login.js
@@ -1,8 +1,10 @@
 import Image from "next/image"
 import LoginLogo from "../assets/siba.png"
-
+import { useMoralis } from "react-moralis";
 
 function Login() {
+    const { authenticate } = useMoralis();
+
     return (
         <div className="bg-black relative ">
             <h1>I am the login screen</h1>
@@ -11,7 +13,7 @@ function Login() {
                 <Image className="object-cover rounded-full " src={LoginLogo} height={180} width={180} />
 
                 {/* Login Button */}
-                <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Login to the Metaverse</button>
+                <button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">어서와</button>
             </div>
             <div className="w-full h-screen">
                 {/* background */}
