8fadba3be1914c2b9fc7132b1df521f8b5c236b1
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2021-12-24 20:45:52 +0900

    login change

--- components/Login.js
+++ components/Login.js
@@ -17,7 +17,7 @@ function Login() {
                 <Image className="object-cover rounded-full " src={LoginLogo} height={180} width={180} />
 
                 {/* Login Button */}
-                <button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">어서와</button>
+                <button onClick={authenticate} className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Welcome</button>
             </div>
             <div className="w-full h-screen">
                 {/* background */}
